47c580ddc227b6b6c0cabd299adaa6abd3fc88cd
Aranxa.C <tassa780@gmail.com>
2020-01-30 10:50:21 +0000

    First Commit

---
+++ meteo.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Météo
+Plugin URI: https://mon-siteweb.com/
+Description: Ceci est mon premier plugin
+Author: Aranxa
+Version: 0.1
+Author URI: aranxa.codina.ovh
+*/
+
+if (!defined('ABSPATH')){
+    die();
+}
